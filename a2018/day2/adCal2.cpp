1e91fccd1197d0f4af4a7596f2fe6bb81d073569
mziolkowski21 <ziolkowskimac21@gmail.com>
2022-12-05 21:52:49 +0100

    day2 2018

--- a2018/day2/adCal2.cpp
+++ a2018/day2/adCal2.cpp
@@ -20,29 +20,31 @@ int main()
     std::string inputString;
     while(input_file >> inputString)
     {
+        bool three = false;
+        bool two = false;
         std::set<char> singleOccurance(begin(inputString),end(inputString));
         std::vector<char> myVec(begin(inputString),end(inputString));
-
-        for(auto elems: myVec)
-        {
-            std::cout << elems;
-        }
-        std::cout << '\n';
+        
+        // for(auto elems: myVec)
+        // {
+        //     std::cout << elems;
+        // }
+        // std::cout << '\n';
 
         for(auto elems: singleOccurance)
         {
             int counter = std::count(myVec.begin(),myVec.end(),elems);
-            if(counter == 3)
+            if(counter == 3 && three==false)
             {
-                //std::cout << elems << " occurs exactly 3 times\n";
+                // std::cout << elems << " occurs exactly 3 times\n";
                 thrice += 1;
-                continue;
+                three = true;
             }
-            else if(counter == 2)
+            else if(counter == 2 && two==false)
             {
-                //std::cout << elems << " occurs exactly 2 times\n";
+                // std::cout << elems << " occurs exactly 2 times\n";
                 twice += 1;
-                break;
+                two = true;
             }
         }
     }
